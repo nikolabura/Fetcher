34071b539261c31d37130c6adc877a7d8e54c112
Nikola B <nbura1@umbc.edu>
2019-03-07 16:53:56 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,157 @@
+// CLASSFETCHER
+// Nikola B.
+
+// Using Renemari Padillo's Discord Bot template
+
+var Discord = require("discord.io");
+var logger = require("winston");
+var auth = require("./auth.json");
+
+var http = require("http");
+
+const CourseDetails = require("./class-object.js");
+
+// CLASSFETCHER CONSTANTS BELOW
+var CATALOG_URL_BASE = "http://catalog.umbc.edu/"
+var SEARCH_URL 	     = "http://catalog.umbc.edu/search_advanced.php?cur_cat_oid=18&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=33&filter[keyword]="
+	// suffix the filter keyword to the end of this URL
+	// no other suffixes required
+// END CONSTANTS
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+	colorize: true
+});
+logger.level = "info";
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+	token: auth.token,
+	autorun: true
+});
+
+logger.info("Starting...");
+
+bot.on("ready", function (evt) {
+	logger.info("Connected");
+	logger.info("Logged in as: ");
+	logger.info(bot.username + " - (" + bot.id + ")");
+});
+
+bot.on("message", function (user, userID, channelID, message, evt) {
+	// Our bot needs to know if it will execute a command
+	// It will listen for messages that will start with `!`
+	if (message.substring(0, 1) == "!") {
+		var args = message.substring(1).split(" ");
+		var cmd = args[0];
+		
+		args = args.splice(1);
+		switch(cmd) {
+			// !ping
+			case "ping":
+				bot.sendMessage({
+					to: channelID,
+					message: "Pong!"
+				});
+				break;
+			// !help
+			case "help":
+				bot.sendMessage({
+					to: channelID,
+					message: "I get info about UMBC courses. Ask @nikolab for further info.\nCommands:\n!ping\n!help\n!class [CLASS]"
+				});
+				break;
+            // !class
+			case "class":
+				console.log("\nGot !class command with " + args.length + " arguments.");
+				if (args.length === 0) {
+					var msg = "**!class**";
+					msg += "\nLooks up information about a class using its ID. Courses are looked up in the UMBC catalog.";
+					msg += "\nThis command has the syntax `!class [CLASS ID]`";
+					msg += "\nExamples: ```\n!class cmsc202\n!class ges110```";
+					msg += "Note: This bot is experimental and will break. Sorry.";
+					bot.sendMessage({
+						to: channelID,
+						message: msg
+					});
+					return;
+				}
+				if (args.length === 2 && /^[A-Za-z]+$/.test(args[0]) && /^\d+$/.test(args[1])) {
+					console.log("Regex test found class ID argument separated in half by a space.");
+					console.log("Condensing both into one argument.");
+					bot.sendMessage({
+						to: channelID,
+						message: "Caution: This command accepts a single-word argument. Your argument has been condensed into \"" + args[0] + args[1] + "\". Searching now..."
+					});
+					var reqUrl = SEARCH_URL + args[0] + args[1];
+				} else {
+					var reqUrl = SEARCH_URL + args[0];
+				}
+				console.log("\nRequesting search results...");
+				console.log(reqUrl);
+				var req = http.request(reqUrl, (res) => {
+					console.log("Recieved search results!");
+					console.log(`STATUS: ${res.statusCode}`);
+					console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+					res.setEncoding('utf8');
+					res.validDataCounter = 0;
+					res.on('data', (chunk) => {
+						console.log("Got data");
+						// Does this chunk have the course match?
+						const CATALOG_MATCH_INDICATOR = "preview_course_nopop";
+						const BAD_RESULTS_RED_FLAG = "coid=\""; // this indicates a blank COID, meaning empty search results!
+						if (chunk.indexOf(CATALOG_MATCH_INDICATOR) !== -1) {
+							console.log("Got chunk containing '" + CATALOG_MATCH_INDICATOR + "'");
+							if (chunk.indexOf(BAD_RESULTS_RED_FLAG) !== -1) {
+								console.log("This chunk also contains '" + BAD_RESULTS_RED_FLAG + "'");
+								console.log("These search results are empty and therefore invalid.");
+								return;
+							}
+							res.validDataCounter += 1;
+							chunkLines = chunk.split("\n");
+							for (var i in chunkLines) {
+								var line = chunkLines[i];
+								var matchIndicatorIndex = line.indexOf(CATALOG_MATCH_INDICATOR);
+								if (matchIndicatorIndex !== -1) {
+									console.log("Processing line...");
+									line = line.substring(matchIndicatorIndex); // chop off beginning
+									line = line.substring(0, line.indexOf('"')); // get line until double quote
+									// now line is something like = preview_course_nopop.php?catoid=18&coid=53775
+									console.log(line);
+									var courseObj = new CourseDetails(args[0], CATALOG_URL_BASE + line, bot, channelID);
+									courseObj.getDetailedCourseData();
+								}
+							}
+							//console.log(`BODY: ${chunk}`);
+						}
+					});
+					res.on('end', () => {
+						console.log("No more data in response.");
+						if (res.validDataCounter == 0) {
+							console.log("No chunks contained a match. Assuming failed search.");
+							var msg = "No classes found. "
+							if (Math.random() > 0.25) {
+								msg += "Please re-check your search parameters.";
+							} else if (Math.random() > 0.03) {
+								msg += "Please re-check your search parameters, 4head.";
+								console.log("Deploying intermediate insult.");
+							} else {
+								msg += "Please re-check your search parameters, you fucking dumbass.";
+								console.log("Deploying devastating insult.");
+							}
+							msg += "\nThis command has the syntax `!class [CLASS ID]`";
+							msg += "\nExample: `!class cmsc202`";
+							bot.sendMessage({
+								to: channelID,
+								message: msg
+							});
+						}
+					});
+				});
+				req.end();
+				break;
+		}
+	}
+});
+
---
+++ class-object.js
@@ -0,0 +1,100 @@
+// this is the object which represents a class
+// not literally a Class as in object oriented
+//	well it is a class but
+// 	you get the point
+
+// it's a class class
+
+const http = require("http");
+const cheerio = require("cheerio");
+
+class CourseDetails {
+	
+	constructor(name, detailsUrl, discordBot, channelID) {
+		this.name = name;
+		this.detailsUrl = detailsUrl;
+		this.discordBot = discordBot;
+		this.channelID = channelID;
+		this.httpResponse = "";
+	}
+	
+	getDetailedCourseData() {
+		console.log("\nRequesting details page...");
+		this.httpResponse = "";
+		var req = http.request(this.detailsUrl, (res) => {
+			console.log("Recieved details!");
+			console.log(`STATUS: ${res.statusCode}`);
+			console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+			res.setEncoding('utf8');
+			res.on('data', (chunk) => {
+				console.log("Got details chunk.");
+				this.httpResponse += chunk;
+			});
+			res.on('end', () => {
+				if (res.statusCode != 200) {
+					console.log("RECIEVED BAD HTTP STATUS CODE, CANCELLING THIS DETAILS PAGE SEQUENCE.");
+					return;
+				}
+				console.log("No more data in details.");
+				console.log("Proceeding to parse details as HTML/DOM.");
+				this.htmlDoc = cheerio.load(this.httpResponse);
+				console.log("Parsing done.");
+				console.log("Finally, printing details to user...");
+				this.printDetails();
+			});
+		});
+		req.end();
+	}
+	
+	printDetails() {
+		if (typeof (this.htmlDoc) === "undefined") {
+			console.log("Member htmlDoc is undefined. Cancelling print.");
+		}
+		var title = this.htmlDoc("#course_preview_title");
+		var parent = title.parent();
+		console.log("Title: " + title.text());
+		console.log("Credits: " + title["0"].next.data);
+		
+		var courseDesc = parent.text(); // start with everything
+		courseDesc = courseDesc.substring(courseDesc.indexOf(")") + 1); // remove prefixed text
+		courseDesc = courseDesc.substring(0, courseDesc.indexOf("Course ID")); // remove suffixed text
+		console.log("Course Description: " + courseDesc);
+		
+		var strongChildren = parent.children("strong");
+		console.log(strongChildren.length);
+		
+		var firstChildren = strongChildren.first().nextUntil("br");
+		for (var i=0; i < firstChildren.length; i++) {
+			console.log(firstChildren.get(i).name);
+		}
+		
+		//console.log(this.htmlDoc("#course_preview_title").nextUntil(':contains("Course ID")').text());
+		
+		/*
+		for (var i=0; i < strongChildren.length; i++) {
+			console.log(i);
+			var nextElement = strongChildren[i].next;
+			if (nextElement.type == "text" && /\S/.test(nextElement.data)) { // NOTE: Short circuit evaluation
+				console.log(nextElement.data);
+			} else {
+				var nextNext = nextElement.next;
+				if (nextNext.type == "tag" && nextNext.name == "a") {
+					console.log(nextNext.children[0].data);
+				}
+			}
+		}*/
+		//console.log(this.htmlDoc.getElementById("course_preview_title"));
+		
+		var chatMessage = "__Found class...__";
+		chatMessage += "\n**Title: " + title.text() + "**";
+		chatMessage += "\n**Credits:** " + title["0"].next.data;
+		chatMessage += "\n**Course Description:** " + courseDesc;
+		this.discordBot.sendMessage({
+			to: this.channelID,
+			message: chatMessage
+		});
+	}
+	
+}
+
+module.exports = CourseDetails;
